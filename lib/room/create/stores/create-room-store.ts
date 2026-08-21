d581f1a759fb95264d5f6bb7b86c92786e87e83c
Hanz <hanznikkolmaas@gmail.com>
2026-08-21 11:52:30 +0800

    fix: data in room creation when refresh

--- lib/room/create/stores/create-room-store.ts
+++ lib/room/create/stores/create-room-store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware"
 import { RoomOption } from "../types/option-types";
 import { PreferenceBudget } from "../types/budget";
 import { LocationStatus } from "../types/location";
@@ -82,77 +83,87 @@ const initialState: CreateRoomState = {
 }
 
 // Store
-export const useCreateRoomStore = create<CreateRoomStore>((set) => ({
-  ...initialState,
-  setHostName: (hostName) => set({ hostName }),
-  setRoomName: (roomName) => set({ roomName }),
-  setMaxParticipants: (value)  => set({maxParticipants: value}),
-  setSelectedCategories: (selectedCategoriesbyNames) => set({ selectedCategoriesbyNames }),
-  toggleCategory: (categoryName: string) => set((state) => {
-    const selected = state.selectedCategoriesbyNames
+export const useCreateRoomStore = create<CreateRoomStore>()(
+  persist(
+    (set) => ({
+      ...initialState,
+      setHostName: (hostName) => set({ hostName }),
+      setRoomName: (roomName) => set({ roomName }),
+      setMaxParticipants: (value)  => set({maxParticipants: value}),
+      setSelectedCategories: (selectedCategoriesbyNames) => set({ selectedCategoriesbyNames }),
 
-    // Remove if already selected
-    if (selected.includes(categoryName)) {
-      return {
-        selectedCategoriesbyNames: selected.filter(
-          (id) => id !== categoryName
-        ),
-      }
-    }
+      toggleCategory: (categoryName: string) => set((state) => {
+        const selected = state.selectedCategoriesbyNames
 
-    // Maximum reached
-    if (selected.length >= 3) {
-      return {
-        selectedCategoriesbyNames: selected
-      }
-    }
+        // Remove if already selected
+        if (selected.includes(categoryName)) {
+          return {
+            selectedCategoriesbyNames: selected.filter(
+              (id) => id !== categoryName
+            ),
+          }
+        }
 
-    // Add category
-    return {
-      selectedCategoriesbyNames: [...selected, categoryName],
-    }
-  }),
-  addOption: (option) => set((state) => ({ options: [...state.options, option], })),
-  editOption: (id:string, title:string) =>
-    set((state) => ({
-      options: state.options.map((option) =>
-        option.option_id === id
-          ? { ...option, title }
-          : option
-      ),
-    })),
-  removeOption: (id) => set((state) => ({ options: state.options.filter((o) => o.option_id !== id) })),
-  clearOptions: () => set({ options: [] }),
-  reset: () => set(initialState),
-  setBudget: (budget) => {set({budget})},
+        // Maximum reached
+        if (selected.length >= 3) {
+          return {
+            selectedCategoriesbyNames: selected
+          }
+        }
 
-  // Location
-  setLocation: (status, data) =>
-    set({
-      locationStatus: status,
-      placeId: data.placeId,
-      placeName: data.placeName,
-      address: data.address,
-      latitude: data.latitude,
-      longitude: data.longitude,
-  }),
-  clearLocation: () =>
-    set({
-      locationStatus: "required",
-      placeId: undefined,
-      placeName: undefined,
-      address: "",
-      latitude: undefined,
-      longitude: undefined,
+        // Add category
+        return {
+          selectedCategoriesbyNames: [...selected, categoryName],
+        }
+      }),
+
+      addOption: (option) => set((state) => ({ options: [...state.options, option], })),
+      editOption: (id:string, title:string) =>
+        set((state) => ({
+          options: state.options.map((option) =>
+            option.option_id === id
+              ? { ...option, title }
+              : option
+          ),
+        })),
+      
+      removeOption: (id) => set((state) => ({ options: state.options.filter((o) => o.option_id !== id) })),
+      clearOptions: () => set({ options: [] }),
+      reset: () => set(initialState),
+      setBudget: (budget) => {set({budget})},
+
+      // Location
+      setLocation: (status, data) =>
+        set({
+          locationStatus: status,
+          placeId: data.placeId,
+          placeName: data.placeName,
+          address: data.address,
+          latitude: data.latitude,
+          longitude: data.longitude,
+      }),
+      clearLocation: () =>
+        set({
+          locationStatus: "required",
+          placeId: undefined,
+          placeName: undefined,
+          address: "",
+          latitude: undefined,
+          longitude: undefined,
+        }),
+      setCoordinates: (latitude, longitude, data) =>
+        set({
+          latitude,
+          longitude,
+          ...(data?.status ? { locationStatus: data.status } : {}),
+          ...(data && "placeId" in data ? { placeId: data.placeId } : {}),
+          ...(data && "placeName" in data ? { placeName: data.placeName } : {}),
+          ...(data && "address" in data ? { address: data.address } : {}),
+        }),
+      setRadius: (radius) => set({ radius }),
     }),
-  setCoordinates: (latitude, longitude, data) =>
-  set({
-    latitude,
-    longitude,
-    ...(data?.status ? { locationStatus: data.status } : {}),
-    ...(data && "placeId" in data ? { placeId: data.placeId } : {}),
-    ...(data && "placeName" in data ? { placeName: data.placeName } : {}),
-    ...(data && "address" in data ? { address: data.address } : {}),
-  }),
-  setRadius: (radius) => set({ radius }),
-}))
+    {
+      name: "spontee-create-room",
+    }
+  )
+)
